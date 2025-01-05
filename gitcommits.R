1b33d334654c3209fbd208385557de464bfba762
guillemmaya92 <guillem.maya92@outlook.com>
2025-01-05 14:28:56 +0100

    cv style commit

--- gitcommits.R
+++ gitcommits.R
@@ -1,5 +1,5 @@
 git remote add origin https://github.com/guillemmaya92/guillemmaya92.github.io.git
 git branch -M main
 git add --all
-git commit -m "initial commit"
+git commit -m "cv style commit"
 git push -u origin main
